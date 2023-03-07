9e60d816013249be92a762da00dfce6716aedf24
uqarni <108438834+uqarni@users.noreply.github.com>
2023-03-07 22:44:33 +0000

    no much done

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import openai
+import pandas as pd
+import os
 
+
+#openai.api_key = os.environ("OPENAI_API_KEY")
+
+#take original csv, make a duplicate, add correct jsonl, save as duplicate jsonl file
+ingest = pd.read_csv('lilearning_t1.csv')
+
+for index, row in ingest.iterrows():
+    print(ingest.loc[index,"unfinished prompt"])
+
+
+
+
+#split into training, validation, and test sets
+#upload training and validation sets to openai
+#fine-tune model
+#query model
